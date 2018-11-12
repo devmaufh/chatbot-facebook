aea97fab67c83f6f3d1d8672579dde15b25c7c19
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-11-11 23:46:40 -0600

    finalMessage changed

--- config/dbConnection.js
+++
@@ -1,9 +0,0 @@
-const mysql=require('mysql');
-module.exoirts()=>{
-    return mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database :'intergrami_web'         
-    });
-}
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
+const mysql=require('mysql');
+const connection=mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '',
+    database:'intergrami_web'
+});
 var express= require('express');
 var bodyParser=require('body-parser');
 var request=require('request');
 //change
-const APP_TOKEN='';
+const APP_TOKEN='EAACZCFcmLbAkBAGrppRZCU1G3yyIPbzbjtawurJ4HlXLV5fmRoWPxrRiqBbI2ZBmNkiR9ua2ojKjZAClOsRZBLlqP3VZAVqgZAN3oyU7dRZB7mizZBjZBYc65ZBLsSRJJO1dmqZCtShYa63MVnj1Phz0hGsaVeZBNDXpC6B6DXLaCvZApJwPqvSrJyKw1u';
 
 var app=express();
 
 app.use(bodyParser.json());
 
 app.listen(3000,function() {
-    console.log("Servidor  en 3000");    
+    console.log("Servidor  en 3000");
+    getOrderInformation(ProductsById(1));
 });
 
-
 app.get('/',function(req,res){
-    res.send("Welcome to chatbot ")
+    res.send('Welcomte to intergrami bot')
 });
 //valida servidor
 app.get('/webhook',function(req,res){
@@ -28,7 +35,7 @@ app.get('/webhook',function(req,res){
 //valida eventos
 app.post('/webhook',function(req,res){
     var data=req.body;
-    
+    console.log('webhook')
     if(data.object=='page'){
         data.entry.forEach(function(pageEntry){
             pageEntry.messaging.forEach(function(messagingEvent){
@@ -45,29 +52,32 @@ app.post('/webhook',function(req,res){
 function reciveMessage(event){
     var senderID=event.sender.id;
     var messageText=event.message.text;
-    console.log(senderID);
-    console.log(messageText);
+    console.log('recive mensaje'+senderID);
+    console.log('recive mensaje'+messageText);
     evaluateMessage(senderID,messageText);
 }
-
 function evaluateMessage(recipientId,message) {
     var finalMessage='';
-    if(isContain(message,'ayuda')){
-        finalMessage='¿En qué puedo ayudarte?';
-    }else{
-        finalMessage='ste man';        
-    }
+    if(isContain(message,'ayuda'))
+         finalMessage='¿En qué puedo ayudarte?\nPor ejemplo puedes:\n1. Consultarme detalles de tus compras: Detalle #numero_de_pedido\n2. Ver el estatus de alguno de tus productos: Producto #numero_producto #token\n 3. Productos nuevos: Novedades';
+    //if(isContain(message,'Producto')
+
+
+
+
     sendMessageText(recipientId,finalMessage);
 }
-
 function sendMessageText(recipientId,message){
     var messageData={
         recipient:{
             id:recipientId
         },
+        message:{
             text:message
         }
-    };
+            //text:message
+        };
+    
     callSendApi(messageData);
 }
 function callSendApi(messageData){
@@ -78,13 +88,32 @@ function callSendApi(messageData){
         json:messageData
     },function(error,response,data){
         if(error){
-            console.log('Error enviando msg');
+            console.log('Error enviando msg'+JSON.stringify(response));
         }else{
-            console.log('Mensaje exitoso');
+            console.log('Mensaje exitoso'+ JSON.stringify(response));
         }
     });
 }
 
 function isContain(sentence,wordToFind){
-    return sentence.indexOf(wordToFind)>-1;
+    return ""+sentence.indexOf(wordToFind)>-1;
+}
+
+function getOrderInformation(sql){
+    connection.connect(function(err){
+        if(err) throw err;
+        connection.query(sql,function(err,result,fields){
+            if(err) throw err;
+            Object.keys(result).forEach(function(key){
+                var row=result[key];
+
+                //setValue(response_query,row.description);
+                //resu=row.description;
+                //console.log("Resultado                  "+resu);
+            });
+        })  
+    });
+}
+function ProductsById(id){
+    return 'Select * from products where id='+id;
 }
